b3def0b727d0f6d77949d31060e88cb579fac963
DKAKI SALAHDDINE <salahddine.dkaki@edu.uiz.ac.ma>
2023-12-13 02:36:42 +0000

    updated

--- project1.py
+++ project1.py
@@ -3,9 +3,9 @@ form tkinter import TK, Entry, Butto, StringVar
 class Calculator:
     def __init__(self, master):
         master.title("Calculator")
-        master.geimetry('357x420+0+0')
+        master.geometry('357x420+0+0')
         master.config(bg='gray')
-        master.resitable(False, False)
+        master.resizable(False, False)
 
         self.equation=StringVar()
         self
