6e7b6ddd800bfa84939480520beb9d1e928eeb3e
DKAKI SALAHDDINE <salahddine.dkaki@edu.uiz.ac.ma>
2023-12-13 16:13:48 +0000

    updated

--- main_app.py
+++ main_app.py
@@ -6,13 +6,10 @@ class Calculator:
         master.iconbitmap('')
         app_width = 357
         app_height = 420
-
         screen_width = root.winfo_screenwidth()
         screen_height = root.winfo_screenheight()
-
         x = (screen_width / 2) - (app_width / 2)
         y = (screen_height / 2) - (app_height / 2)
-
         master.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
         my_label = Label(root, text =f'Width:{screen_width} Height:{screen_height}')
         my_label.pack(pady=20)
