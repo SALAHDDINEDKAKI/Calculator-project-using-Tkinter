075c1ba25cd5418a87ae2a53d2e0e372a70f93d5
DKAKI SALAHDDINE <salahddine.dkaki@edu.uiz.ac.ma>
2023-12-13 02:35:09 +0000

    updated

--- project1.py
+++ project1.py
@@ -1,1 +1,14 @@
-def zzzeze
+form tkinter import TK, Entry, Butto, StringVar
+
+class Calculator:
+    def __init__(self, master):
+        master.title("Calculator")
+        master.geimetry('357x420+0+0')
+        master.config(bg='gray')
+        master.resitable(False, False)
+
+        self.equation=StringVar()
+        self
+
+root=TK()
+root.mainloop()
